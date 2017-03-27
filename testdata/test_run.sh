8e0ba9ddc6141d0dd8f3e5247b7da31dddc3404e
Sascha Steinbiss <sascha.steinbiss@dcso.de>
2017-03-27 14:22:01 +0200

    extend tests

---
+++ testdata/test_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+
+
+if [ -z ${TIE_TOKEN+x} ]; then
+   # TIE_TOKEN is not set, simply checking whether tool can run
+   ./tie2misp --help
+else
+    sed "s/SETME/$TIE_TOKEN/" testdata/config.yml > settings/config.yml
+    ./tie2misp c2server --date 2017-03-13 --noupload --file | tail -n 1 | xargs jq '.Event.Attribute | length' > length
+    test `cat length` -gt "0"
+    rm -f settings/config.yml
+fi
